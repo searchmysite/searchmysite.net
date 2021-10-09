83966245bf4dfc1a43ddc295adf9962baa027081
michael-lewis <michael@michael-lewis.com>
2021-10-09 13:44:39 +0100

    Bulk import scripts for #10: Index wikipedia

--- src/indexer/bulkimport/wikipedia/import.sh
+++ src/indexer/bulkimport/wikipedia/import.sh
@@ -1,18 +1,67 @@
 #!/bin/bash
 
-echo "Step 1: Download"
-cd cirrussearch
-wget https://dumps.wikimedia.org/other/cirrussearch/20201214/enwiki-20201214-cirrussearch-content.json.gz
-# A couple of hours to download, and approx 30Gb.
+# Search extracts available at https://dumps.wikimedia.org/other/cirrussearch/
+# e.g. https://dumps.wikimedia.org/other/cirrussearch/current/enwiki-20210927-cirrussearch-content.json.gz
+# Look like they're updated approx weekly. Also a significantly smaller test file at
+# e.g. https://dumps.wikimedia.org/other/cirrussearch/current/testwiki-20210927-cirrussearch-content.json.gz
 
-echo "Step 2: Uncompress"
-gunzip enwiki-20201214-cirrussearch-content.json.gz
-# Takes about 30 mins to uncompress, to approx 125Gb.
-# Split the file into 500 line chunks
+# Language code
+LAN_CODE=en
 
-echo "Step 3"
-mkdir unprocessed
-cd unprocessed
-split -a 6 -l 500 ../enwiki-20201214-cirrussearch-content.json enwiki-
-# Takes about 47 mins.
+FOLDER=cirrussearch/${LAN_CODE}
+SOLR_URL=http://search:8983/solr/content/
 
+mkdir -p $FOLDER
+
+
+echo -ne "Step 1: Find the last successful wikipedia import, and see if there is a later one available"
+LATEST_COMPLETED=`python -c 'import wikiutils; c = wikiutils.latest_complete(); print(c)'`
+if [[ $LATEST_COMPLETED == "" ]]; then
+    LATEST_COMPLETED=00000000
+fi
+echo -ne "\nLast completed $LATEST_COMPLETED"
+LATEST_AVAILABLE=`python -c 'import wikiutils; a = wikiutils.latest_available(); print(a)'`
+echo -ne "\nLast available $LATEST_AVAILABLE"
+if [[ $LATEST_COMPLETED -ge $LATEST_AVAILABLE ]]; then
+    echo -ne "\nThere isn't a later import available"
+    exit 0
+fi
+URL_ROOT=https://dumps.wikimedia.org/other/cirrussearch/${LATEST_AVAILABLE}/
+COMPRESSED_FILE=${LAN_CODE}wiki-${LATEST_AVAILABLE}-cirrussearch-content.json.gz
+
+
+echo -ne "\nStep 2: Download, started at" `date`
+python -c "import wikiutils; wikiutils.update_log(\"RUNNING\", \"Using export: ${LATEST_AVAILABLE}\")"
+if [ -f "${FOLDER}/$COMPRESSED_FILE" ]; then
+    echo -ne "${FOLDER}/$COMPRESSED_FILE exists. Skipping download."
+else 
+    wget -O ${FOLDER}/${COMPRESSED_FILE} ${URL_ROOT}${COMPRESSED_FILE}
+fi
+
+
+echo -ne "\nStep 3: Uncompress and split into smaller chunks, started at" `date`
+gunzip -c ${FOLDER}/$COMPRESSED_FILE | split -a 6 -l 500 - ${FOLDER}/${LAN_CODE}wiki-
+rm ${FOLDER}/$COMPRESSED_FILE # Delete downloaded file to save space
+
+
+echo -ne "\n\nStep 4: Reformat, started at" `date`
+for file in ${FOLDER}/*
+do
+    echo $file
+    python reformatjson.py $file $LAN_CODE
+done
+
+
+echo -ne "\n\nStep 5: Load into Solr, started at" `date` "\n"
+# Start by deleting existing Solr collection, for a clean index
+# If allowing indexing multiple languages, delete query would be <delete><query>(domain:wikipedia.org)AND(language:${LAN_CODE})</query></delete>
+curl ${SOLR_URL}update?commit=true -H "Content-Type: text/xml" --data-binary '<delete><query>domain:wikipedia.org</query></delete>'
+for file in ${FOLDER}/*
+do
+    echo $file
+    curl ${SOLR_URL}update?commit=true -H "Content-Type: application/json" --data-binary @$file
+done
+rm ${FOLDER}/*
+
+echo -ne "\nCompleted at" `date` "\n"
+python -c "import wikiutils; wikiutils.update_log(\"COMPLETE\", \"Using export: ${LATEST_AVAILABLE}\")"
--- src/indexer/bulkimport/wikipedia/reformatjson.py
+++ src/indexer/bulkimport/wikipedia/reformatjson.py
@@ -1,10 +1,40 @@
 import json
-from indexer.util import convert_datetime_to_utc_date
+from datetime import datetime
+import sys
+import os
 
-file = "cirrussearch/unprocessed/enwikiaaaaaaaaaa"
-linkroot = "https://en.wikipedia.org/wiki/"
+# Change the sys path so we can import utilities common to both the src/indexer/bulkimport and src/indexer/indexer 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from common import config
+from common import utils
 
-# File is in the format
+# Get command line parameters, i.e. file and language
+try:
+    input_file = sys.argv[1] 
+except IndexError:
+    print("Please specify a file to process and optional language, e.g. python reformatjson.py cirrussearch/en/enwiki-aaaaaa en")
+    sys.exit()
+try:
+    lang = sys.argv[2]
+except IndexError:
+    lang = "en"
+
+# Get list of indexed domains and list of domains which allow subdomains 
+domains = utils.get_all_domains()
+domains_allowing_subdomains = utils.get_domains_allowing_subdomains()
+
+# Get indexed outlinks
+domain = "wikipedia.org"
+all_indexed_inlinks = utils.get_all_indexed_inlinks_for_domain(domain)
+
+# Other variables
+output_file = input_file # Overwrite existing file when done to save disk space
+#output_file = input_file + ".json" # Create new output file, useful for dev testing
+linkroot = "https://" + lang + "." + domain + "/wiki/"
+
+
+# Step 1: Make valid JSON
+# input_file is in the format
 """
 {
     "index": {
@@ -45,50 +75,22 @@ linkroot = "https://en.wikipedia.org/wiki/"
 # {'index': {...}, 'template': [...], ... },
 # {'index': {...}, 'template': [...], ... }
 # ]
-inputPages = []
-with open(file) as f:
-    for jsonObj in f:
+input_pages = []
+with open(input_file) as infile:
+    for jsonObj in infile:
         j = json.loads(jsonObj)
         if "index" in j:
             page = dict()
             page.update(j)
-            inputPages.append(page)
+            input_pages.append(page)
         else:
-            page = inputPages[-1]
+            page = input_pages[-1]
             page.update(j)
 
-# Mapping between searchmysite.net Solr schema and wikipedia cirrussearch dump:
-# id -> same as url
-# url -> inputPage["title"] transformed accordingly (see comments below)
-# domain -> always wikipedia.org
-# is_home -> always False unless title="Main Page"
-# title -> inputPage["title"]
-# author -> n/a
-# description -> n/a
-# tags -> n/a
-# body -> likely to be deprecated so don't populate
-# content -> inputPage["text"]
-# page_type -> appears to be "website" for all pages so no real value in adding
-# page_last_modified -> inputPage["timestamp"] . Might already be in Solr's DateTimeFormatter.ISO_INSTANT format, i.e. YYYY-MM-DDThh:mm:ssZ, e.g. 2021-07-02T18:37:57Z
-# published_date -> n/a
-# indexed_date -> convert_datetime_to_utc_date(datetime.datetime.now())
-# date_domain_added -> only set if is_home = True. Could be hardcoded for now, but should be "SELECT date_domain_added FROM tblIndexedDomains WHERE domain = 'wikipedia.org';"
-# site_category -> always "independent-website"
-# site_last_modified -> TBC, perhaps use the timestamp from the import (same value for all pages in a site)
-# owner_verified -> assumed to be False for now (but should in future could be a database lookup to confirm)
-# contains_adverts -> assume False
-# api_enabled -> only set if is_home = True. assume False
-# language -> inputPage["language"]
-# language_primary -> inputPage["language"][:2]
-# indexed_inlinks -> it would be the query http://localhost:8983/solr/content/select?q=*%3A*&fq=indexed_outlinks%3A*wikipedia.org*&fl=url,indexed_outlinks&rows=10000 
-#                    but wikipedia.org needs to be added to the SELECT DISTINCT domain FROM tblIndexedDomains; to appear in other pages indexed_outlinks first
-# indexed_inlinks_count -> 
-# indexed_inlink_domains -> 
-# indexed_inlink_domains_count ->
-# indexed_outlinks -> get domains_for_indexed_links, and check all external_link for a link on a domain in domains_for_indexed_links
 
-outputPages = []
-for inputPage in inputPages:
+# Step 2: Create new JSON
+output_pages = []
+for input_page in input_pages:
     page = dict()
     # Converting titles to urls:
     # From https://en.wikipedia.org/wiki/Help:URL
@@ -97,22 +99,65 @@ for inputPage in inputPages:
     # Preludes (Rachmaninoff) -> https://en.wikipedia.org/wiki/Preludes_(Rachmaninoff)
     # The Vampyr: A Soap Opera -> https://en.wikipedia.org/wiki/The_Vampyr:_A_Soap_Opera
     # Meisterstück -> https://en.wikipedia.org/wiki/Meisterst%C3%BCck
-    # Princess of Wales's Stakes -> https://en.wikipedia.org/wiki/Princess_of_Wales%27s_Stakes (conversely outgoing_link is e.g. Princess_of_Wales's_Stakes)
-    page['id'] = "https://en.wikipedia.org/wiki/" + inputPage["title"].replace(" ", "_")
-    page['url'] = page['id']
-    page['domain'] = "wikipedia.org"
-    if inputPage["title"] == 'Main Page':
-        page['is_home'] = True
-        # date_domain_added -> only set if is_home = True. Could be hardcoded for now, but should be "SELECT date_domain_added FROM tblIndexedDomains WHERE domain = 'wikipedia.org';"
-        page[' api_enabled'] = False
+    # Princess of Wales's Stakes -> https://en.wikipedia.org/wiki/Princess_of_Wales%27s_Stakes (although outgoing_link is e.g. Princess_of_Wales's_Stakes)
+    if 'title' in input_page: # Don't do any of this if there's not a title
+        page['id'] = linkroot + input_page["title"].replace(" ", "_")
+        page['url'] = page['id']
+        page['domain'] = domain
+        if input_page["title"] == 'Main Page':
+            page['is_home'] = True
+            # Original wikipedia.org submission date was "2020-11-25T09:43:50.344Z"
+            # That is almost the correct format for Solr - just need to remove the .344
+            # Hardcoding date_domain_added for now, but should be "SELECT date_domain_added FROM tblIndexedDomains WHERE domain = 'wikipedia.org';" in case it changes
+            page['date_domain_added'] = '2020-11-25T09:43:50Z'
+            page['api_enabled'] = False
+        else:
+            page['is_home'] = False
+        page['title'] = input_page["title"]
+        # author, description & tags -> n/a. body -> likely to be deprecated so don't populate
+        page['content'] = input_page["text"]
+        # page_type -> appears to be "website" for all pages so no real value in adding
+        page['page_last_modified'] = input_page["timestamp"] # Looks like it is already in Solr's DateTimeFormatter.ISO_INSTANT format, i.e. YYYY-MM-DDThh:mm:ssZ, e.g. 2021-07-02T18:37:57Z
+        # published_date -> n/a
+        page['indexed_date'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        page['site_category'] = "independent-website"
+        #page['site_last_modified'] = "" # TBC, but not currently populated elsewhere. Perhaps use the timestamp from the import (same value for all pages in a site)
+        page['owner_verified'] = False # assumed to be False for now (but should in future could be a database lookup to confirm)
+        page['contains_adverts'] = False # contains_adverts -> assume False
+        if 'language' in input_page:
+            page['language'] = input_page["language"]
+            page['language_primary'] = input_page["language"][:2]
+        # Populate indexed_inlinks, indexed_inlinks_count, indexed_inlink_domains and indexed_inlink_domains_count
+        # i.e. the links and domains with links to this page 
+        if page['url'] in all_indexed_inlinks:
+            page['indexed_inlinks'] = all_indexed_inlinks[page['url']]
+        if 'indexed_inlinks' in page and len(page['indexed_inlinks']) > 0:
+            page['indexed_inlinks_count'] = len(page['indexed_inlinks'])
+        indexed_inlink_domains = []
+        if 'indexed_inlinks' in page:
+            for indexed_inlink in page['indexed_inlinks']:
+                indexed_inlink_domain = utils.extract_domain_from_url(indexed_inlink, domains_allowing_subdomains)
+                if indexed_inlink_domain not in indexed_inlink_domains:
+                    indexed_inlink_domains.append(indexed_inlink_domain)
+        page['indexed_inlink_domains'] = indexed_inlink_domains
+        if len(indexed_inlink_domains) > 0:
+            page['indexed_inlink_domains_count'] = len(indexed_inlink_domains)
+        # Populate the indexed_outlinks, i.e. pages on other domains within this index to which this page links
+        # Note that the mechanism here isn't bulletproof - it just looks for the domain string anywhere rather than between the "//" and first "/"
+        # But given it will be run 10s of millions of times for potentially 100s of links on potentially 1000s of domains
+        # we want it to be fairly fast 
+        indexed_outlinks = []
+        if domains and "external_link" in input_page:
+            links = input_page["external_link"]
+            for link in links:
+                if any(domain in link for domain in domains): 
+                    indexed_outlinks.append(link)
+        page['indexed_outlinks'] = indexed_outlinks
+        output_pages.append(page)
     else:
-        page['is_home'] = False
-    page['title'] = inputPage["title"]
-    #page['content'] = inputPage["text"]
-    page['page_last_modified'] = inputPage["timestamp"]
-    page['indexed_date'] = convert_datetime_to_utc_date(datetime.datetime.now())
-    page['language'] = inputPage["language"]
-    page['language_primary'] = inputPage["language"][:2]
-    outputPages.append(page)
+        print("Page id {} from file {} was not imported due to missing title: ".format(input_page['index']['_id'], input_file))
 
-print(outputPages)
+
+# Step 3: Save new JSON
+with open(output_file, 'w') as outfile:
+    json.dump(output_pages, outfile)
