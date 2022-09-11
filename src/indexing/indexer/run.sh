b9832a738d90ac3ab6e8b9e32a72e77d5e888b83
michael-lewis <michael@michael-lewis.com>
2022-09-11 09:57:39 +0100

    Add a small sleep before first starting, to avoid a potential error if the database has not started up yet

--- src/indexing/indexer/run.sh
+++ src/indexing/indexer/run.sh
@@ -5,7 +5,7 @@ S=120
 if [ "$1" = "dev" ]
 then
   S=60
-  echo "Running in dev mode. Going to run search_my_site_scheduler.py more quickly"
+  echo "Running in dev mode. Going to run search_my_site_scheduler.py every 60s rather than every 120s."
 elif [ "$1" = "test" ]
 then
   echo "Running in test mode. Not going to run search_my_site_scheduler.py"
@@ -13,6 +13,9 @@ then
     do sleep $S
   done
 fi
+
+# Give the database etc. time to start
+sleep $S
 
 while true
   do python /usr/src/app/search_my_site_scheduler.py
