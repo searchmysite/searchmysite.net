8dd8e2a8d0677c4761d652f317faae47c9e345f8
michael-lewis <michael@michael-lewis.com>
2022-10-15 18:41:10 +0100

    Added test cases for page and resultsperpage parameters, to catch issues like #78, and checked response is valid json and that the params in the response matches those in the request, to improve #76

--- tests/web/integration/test_5_api.py
+++ tests/web/integration/test_5_api.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 
 def test_api_basic(anon_client, add_basic_details):
     response = anon_client.get('/api/v1/search/' + pytest.add_basic_domain + '?q=*')
@@ -8,5 +9,17 @@ def test_api_basic(anon_client, add_basic_details):
 def test_api_full(anon_client, add_full_details):
     response = anon_client.get('/api/v1/search/' + pytest.add_full_domain + '?q=*')
     assert response.status_code == 200
-    assert b"\"results\":" in response.data
+    assert b"\"results\":" in response.data # check that there are results
+    assert json.loads(response.data) # check that it is valid json
+
+def test_api_full_and_params(anon_client, add_full_details):
+    q = 'search'
+    page = 2
+    resultsperpage = 2
+    response = anon_client.get('/api/v1/search/{}?q={}&page={}&resultsperpage={}'.format(pytest.add_full_domain, q, page, resultsperpage))
+    params = {'q': q, 'page': page, 'resultsperpage': resultsperpage}
+    json_params = json.dumps(params)
+    json_response = json.loads(response.data)
+    assert response.status_code == 200
+    assert json.dumps(json_response['params']) == json_params
 
