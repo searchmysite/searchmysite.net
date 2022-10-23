8791b546501e50e374c30507f3c2cd663d49b990
michael-lewis <michael@michael-lewis.com>
2022-10-23 17:46:17 +0100

    Updated test to reflect new wording on new combined results page

--- tests/web/integration/test_4_search.py
+++ tests/web/integration/test_4_search.py
@@ -3,9 +3,9 @@ import pytest
 def test_search(anon_client, add_basic_details, add_full_details):
     response = anon_client.get('/search/') # Going to /search/ without a query defaults to a search for everything, i.e. query = * 
     assert response.status_code == 200
-    assert b"<title>Search My Site - Results</title>" in response.data
-    assert b"results for <em>query = *</em>" in response.data # This is the message shown when there are 1 or more results
-    assert b"No results found for <em>query = *</em>" not in response.data # This is the message shown when there are no results
+    assert b"<title>Search My Site - Search Results</title>" in response.data
+    assert b" results" in response.data # This is the message shown when there are 1 or more results
+    assert b"No results found for " not in response.data # This is the message shown when there are no results
     assert b"searchmysite.net is an open source search engine and search as a service" not in response.data # This is the text on the home page, i.e. /, rather than /search/
     assert bytes('class="result-link">{}</a>'.format(pytest.add_basic_home_page).encode('utf-8')) in response.data
     assert bytes('class="result-link">{}</a>'.format(pytest.add_full_home_page).encode('utf-8')) in response.data
@@ -13,6 +13,6 @@ def test_search(anon_client, add_basic_details, add_full_details):
 def test_browse(anon_client):
     response = anon_client.get('/search/browse/')
     assert response.status_code == 200
-    assert b"<title>Search My Site - Browse</title>" in response.data
+    assert b"<title>Search My Site - Browse Sites</title>" in response.data
     assert bytes('class="result-link">{}</a>'.format(pytest.add_basic_domain).encode('utf-8')) in response.data
     assert bytes('class="result-link">{}</a>'.format(pytest.add_full_domain).encode('utf-8')) in response.data
