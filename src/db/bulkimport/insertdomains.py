a9da3060c6cd9285ef2bd54423a59e2acf3e0803
michael-lewis <michael@michael-lewis.com>
2023-01-28 12:27:18 +0000

    Updated to work with new database schema introduced in v1.1.0

--- src/db/bulkimport/insertdomains.py
+++ src/db/bulkimport/insertdomains.py
@@ -4,6 +4,10 @@ import psycopg2.extras
 import sys
 import os
 
+
+# Instructions:
+# See comments at the start of checkdomains.py
+
 if len(sys.argv) > 1:
     inp = sys.argv[1]
 else:
@@ -21,20 +25,32 @@ if not POSTGRES_PASSWORD:
 input_file = 'data/' + inp + '.json'
 
 database_host = "db" # Dev database
-#database_host = "searchmysite.net" # Prod database
+#database_host = "142.132.178.149" # Prod database
 
-# moderator_approved not set, i.e. null, so it will require review
-sql_pending = "INSERT INTO tblDomains "\
-    "(domain, home_page, date_domain_added, owner_submitted, submission_method) "\
-    "VALUES ((%s), (%s), now(), FALSE, 'SQL');"
+# This is copied from ../../web/content/dynamic/searchmysite/sql.py - if that is updated this should be too
+sql_insert_domain = "INSERT INTO tblDomains "\
+    "(domain, home_page, domain_first_submitted, category, include_in_public_search, indexing_type) "\
+    "VALUES ((%s), (%s), NOW(), (%s), TRUE, 'spider/default');"
 
-sql_indexed = "INSERT INTO tblDomains "\
-    "(domain, home_page, date_domain_added, owner_verified, validation_method, "\
-    "expire_date, api_enabled, indexing_type, indexing_frequency, indexing_page_limit, indexing_current_status, indexing_status_last_updated), "\
-    "moderator_approved, moderator_action_date, indexing_enabled "\
-    "VALUES ((%s), (%s), now(), FALSE, 'SQL', "\
-    "now() + '1 year', FALSE, 'spider/default', '7 days', 50, 'PENDING', now()), "\
-    "TRUE, now(), TRUE;"
+sql_insert_basic_listing = "INSERT INTO tblListingStatus (domain, tier, status, status_changed, pending_state, pending_state_changed) "\
+    "VALUES ((%s), (%s), 'PENDING', NOW(), 'MODERATOR_REVIEW', NOW());"
+
+sql_update_basic_approved = "UPDATE tblListingStatus "\
+    "SET status = 'ACTIVE', status_changed = NOW(), pending_state = NULL, pending_state_changed = NOW(), listing_start = NOW(), listing_end = NOW() + (SELECT listing_duration FROM tblTiers WHERE tier = 1) "\
+    "WHERE domain = (%s) AND status = 'PENDING' AND tier = 1 AND pending_state = 'MODERATOR_REVIEW'; "\
+    "UPDATE tblDomains SET "\
+    "moderator_approved = TRUE, "\
+    "moderator = (%s), "\
+    "full_reindex_frequency = tblTiers.default_full_reindex_frequency, "\
+    "incremental_reindex_frequency = tblTiers.default_incremental_reindex_frequency, "\
+    "indexing_page_limit = tblTiers.default_indexing_page_limit, "\
+    "on_demand_reindexing = tblTiers.default_on_demand_reindexing, "\
+    "api_enabled = tblTiers.default_api_enabled, "\
+    "indexing_enabled = TRUE, "\
+    "indexing_status = 'PENDING', "\
+    "indexing_status_changed = NOW() "\
+    "FROM tblTiers WHERE tblTiers.tier = 1 and tblDomains.domain = (%s);"
+
 
 # If valid and reviewed are True and already_in_list and already_in_database are False
 # this will insert straight into tblDomains with moderator_approved TRUE for indexing
@@ -42,29 +58,29 @@ sql_indexed = "INSERT INTO tblDomains "\
 # this will insert into tblDomains with moderator_approved NULL, requiring manual inspection prior to 
 # approval (or rejection)
 
-def insert_pending(domain, home_page):
+def insert_domain(domain, home_page, category, tier):
     conn = psycopg2.connect(host=database_host, dbname="searchmysitedb", user="postgres", password=POSTGRES_PASSWORD)
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute(sql_pending, (domain, home_page, ))
+    cursor.execute(sql_insert_domain, (domain, home_page, category))
+    cursor.execute(sql_insert_basic_listing, (domain, tier))
     conn.commit()
-    print("insert pending", domain, home_page)
+    print("inserting domain: {}, home page: {}, category: {}, tier: {}".format(domain, home_page, category, tier))
 
-def insert_indexed(domain, home_page):
-    conn = psycopg2.connect(host="searchmysite.net", dbname="searchmysitedb", user="postgres", password=POSTGRES_PASSWORD)
+def approve_domain(domain, moderator):
+    conn = psycopg2.connect(host=database_host, dbname="searchmysitedb", user="postgres", password=POSTGRES_PASSWORD)
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute(sql_indexed, (domain, home_page, ))
+    cursor.execute(sql_update_basic_approved, (domain, moderator, domain))
     conn.commit()
-    print("insert indexed", domain, home_page)
+    print("approving domain: {}, moderator: {}".format(domain, moderator))
 
 with open(input_file) as json_file:
     domains = json.load(json_file)
     for domain in domains:
         if domain['valid'] == True: # If not valid the already_in_list and already_in_database check won't have been performed
             if domain['already_in_list'] == False and domain['already_in_database'] == False:
+                insert_domain(domain['domain'], domain['home_page'], 'personal-website', 1) # category and tier hardcoded
                 if domain['reviewed'] == True:
-                    insert_indexed(domain['domain'], domain['home_page'])
-                elif domain['reviewed'] == False:
-                    insert_pending(domain['domain'], domain['home_page'])
+                    approve_domain(domain['domain'], 'michael-lewis.com') # moderator hardcoded
                 else:
                     print("Issue with {}".format(domain['domain']))
             else:
