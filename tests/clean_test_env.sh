2cb73e54586dd1c40e93eba8d3bbd379fef310c2
michael-lewis <michael@michael-lewis.com>
2023-07-08 12:46:39 +0100

    Added sleep to give the environment time to start up (tests were sometimes failing because run_tests.sh was run before the environment had started up)

--- tests/clean_test_env.sh
+++ tests/clean_test_env.sh
@@ -5,6 +5,6 @@ docker-compose -f docker-compose.test.yml build
 # Note: if you need a rebuild, you may need to use the following instead, but this is not enabled by default because it takes several minutes 
 #docker-compose -f docker-compose.test.yml build --no-cache
 docker-compose -f docker-compose.test.yml up -d
+sleep 10
 cd ~/projects/searchmysite.net/tests
-
 
