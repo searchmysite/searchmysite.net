afa35379d8d114de1b3b9d946586ba4442823eb3
Michael Lewis <michael@michael-lewis.com>
2026-07-15 16:35:18 +0100

    Implemented #186: Decrease DOWNLOAD_DELAY from 2 to 1

--- src/indexing/indexer/settings.py
+++ src/indexing/indexer/settings.py
@@ -48,7 +48,7 @@ CONCURRENT_REQUESTS = 16
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 2
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 4
 #CONCURRENT_REQUESTS_PER_IP = 16
