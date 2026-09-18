9f7faef88137af03388edd50febaf4da919ee2d4
Michael Lewis <michael@michael-lewis.com>
2026-09-18 10:52:30 +0100

    New test cases for new API endpoint for #191: Component to make it easier to add a search box to sites

--- tests/web/integration/test_5_api.py
+++ tests/web/integration/test_5_api.py
@@ -22,3 +22,25 @@ def test_api_full_and_params(anon_client, add_full_details):
     assert response.status_code == 200
     assert json.dumps(json_response['params'], sort_keys=True) == json.dumps(params, sort_keys=True)
 
+def test_status_basic(anon_client, add_basic_details):
+    response = anon_client.get('/api/v1/status/' + pytest.add_basic_domain)
+    json_response = json.loads(response.data)
+    assert response.status_code == 200
+    assert json_response['domain'] == pytest.add_basic_domain
+    assert json_response['tier'] == 1
+    assert json_response['api_enabled'] is False
+    assert 'max-age=3600' in response.headers['Cache-Control']
+
+def test_status_full(anon_client, add_full_details):
+    response = anon_client.get('/api/v1/status/' + pytest.add_full_domain)
+    json_response = json.loads(response.data)
+    assert response.status_code == 200
+    assert json_response['domain'] == pytest.add_full_domain
+    assert json_response['tier'] == 3
+    assert json_response['api_enabled'] is True
+
+def test_status_unknown(anon_client, add_basic_details):
+    response = anon_client.get('/api/v1/status/not-a-real-domain-xyz.com')
+    assert response.status_code == 404
+    assert b"not found" in response.data # Full response {"message": "Domain not-a-real-domain-xyz.com not found"}
+
